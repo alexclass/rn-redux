a36db6354b8ec459d87dd9ea1bdaedcd85abecc2
alex Davis <alex@alex2006hw.com>
2016-12-28 04:28:39 -0800

    lecture-102

--- 03.tech_stack/src/reducers/SelectionReducer.js
+++ 03.tech_stack/src/reducers/SelectionReducer.js
@@ -3,7 +3,7 @@ export default (state = null, action) => {
     console.log('1.SelectionReducer action: ', action);
     switch (action.type) {
         case 'select_library':
-        console.log('2.SelectionReducer switch action.type: ', action.type);
+            console.log('2.SelectionReducer switch action.type: ', action.type);
             return action.payload;
         default:
             return state;
